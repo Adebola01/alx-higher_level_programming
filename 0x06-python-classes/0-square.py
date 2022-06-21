a446c2cfe46d2097feda3fa28a4e13a591f063e3
Your Name <you@example.com>
2022-06-21 14:50:02 +0000

    Write an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# 0-square.py
+
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
+
+"""Define a class Square."""
+
+
+
+
+
+class Square:
+
+    """Represent a square."""
+
+    pass
